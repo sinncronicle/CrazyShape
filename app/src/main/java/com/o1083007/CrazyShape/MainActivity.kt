d02faceeca556373d2a562df5944928d1d6a85e9
sinncronicle <o1083007@gm.pu.edu.tw>
2021-05-24 13:19:56 +0800

    2021上機考v3_(林施良)

--- app/src/main/java/com/o1083007/CrazyShape/MainActivity.kt
+++ app/src/main/java/com/o1083007/CrazyShape/MainActivity.kt
@@ -18,7 +18,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val shape = intArrayOf(R.drawable.circle, R.drawable.square, R.drawable.triangle, R.drawable.star)
 
+        val i:Int = (0..3).random()
+        imgNext.setImageResource(shape[i])
 
                 val img: ImageView = findViewById(R.id.imgTitle)
                 GlideApp.with(this)
