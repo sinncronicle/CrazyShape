93488e5fbdf1c1e703a3ce044004e12b273fd7f8
sinncronicle <o1083007@gm.pu.edu.tw>
2021-05-24 13:13:40 +0800

    2021上機考v2_(林施良)

--- app/src/main/java/com/o1083007/CrazyShape/MainActivity.kt
+++ app/src/main/java/com/o1083007/CrazyShape/MainActivity.kt
@@ -4,13 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import kotlinx.android.synthetic.main.activity_main.*
+
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+                val img: ImageView = findViewById(R.id.imgTitle)
+                GlideApp.with(this)
+                    .load(R.drawable.cover)
+                    .override(800,600)
+                    .into(img)
+
         Toast.makeText(this, "作者:林施良", Toast.LENGTH_LONG).show()
         imgNext.setOnLongClickListener(object: View.OnLongClickListener{
             override fun onLongClick(v: View?): Boolean {
